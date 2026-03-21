8c7dced5ff8f637ef3f40b52c7bd61272c71454c
mmms914 <mikhail.khadzhaev832@gmail.com>
2026-03-21 23:53:27 +0700

    [tests] add mocks

---
+++ internal/service/booking/mocks/mocks.go
@@ -0,0 +1,6 @@
+package mocks
+
+//go:generate go run github.com/vektra/mockery/v2@latest --name=Repository --filename=repository_mock.go --dir=../ --output=.
+//go:generate go run github.com/vektra/mockery/v2@latest --name=SlotRepository --filename=slot_repository_mock.go --dir=../ --output=.
+//go:generate go run github.com/vektra/mockery/v2@latest --name=UserRepository --filename=user_repository_mock.go --dir=../ --output=.
+//go:generate go run github.com/vektra/mockery/v2@latest --name=LinkManager --filename=link_manager_mock.go --dir=../ --output=.
---
+++ internal/service/slot/mocks/mocks.go
@@ -0,0 +1,4 @@
+package mocks
+
+//go:generate go run github.com/vektra/mockery/v2@latest --name=Repository --filename=repository_mock.go --dir=../ --output=.
+//go:generate go run github.com/vektra/mockery/v2@latest --name=RoomRepository --filename=room_repository_mock.go --dir=../ --output=.
