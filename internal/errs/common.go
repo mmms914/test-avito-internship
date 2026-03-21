7591343eaf01d2dee2bd1e9404380d9625c9648f
mmms914 <mikhail.khadzhaev832@gmail.com>
2026-03-21 21:04:13 +0700

    [ref] updated errors

--- internal/errs/common.go
+++ internal/errs/common.go
@@ -1,5 +1,18 @@
 package errs
 
+const (
+	InvalidRequestErrorCode    string = "INVALID_REQUEST"
+	UnauthorizedErrorCode      string = "UNAUTHORIZED"
+	NotFoundErrorCode          string = "NOT_FOUND"
+	RoomNotFoundErrorCode      string = "ROOM_NOT_FOUND"
+	SlotNotFoundErrorCode      string = "SLOT_NOT_FOUND"
+	SlotAlreadyBookedErrorCode string = "SLOT_ALREADY_BOOKED"
+	BookingNotFoundErrorCode   string = "BOOKING_NOT_FOUND"
+	ForbiddenErrorCode         string = "FORBIDDEN"
+	ScheduleExistsErrorCode    string = "SCHEDULE_EXISTS"
+	InternalErrorCode          string = "INTERNAL_ERROR"
+)
+
 type CustomError struct {
 	code    string
 	message string
---
+++ internal/errs/errors.go
@@ -0,0 +1,14 @@
+package errs
+
+var (
+	ErrDayIsInvalid      = NewCustomError(InvalidRequestErrorCode, "day should be between 1 and 7")
+	ErrRoomNotExists     = NewCustomError(RoomNotFoundErrorCode, "room does not exist")
+	ErrUnauthorized      = NewCustomError(UnauthorizedErrorCode, "unauthorized")
+	ErrNotFound          = NewCustomError(NotFoundErrorCode, "not found")
+	ErrSlotNotFound      = NewCustomError(SlotNotFoundErrorCode, "slot not found")
+	ErrSlotAlreadyBooked = NewCustomError(SlotAlreadyBookedErrorCode, "slot already booked")
+	ErrBookingNotFound   = NewCustomError(BookingNotFoundErrorCode, "booking not found")
+	ErrForbidden         = NewCustomError(ForbiddenErrorCode, "forbidden")
+	ErrScheduleExists    = NewCustomError(ScheduleExistsErrorCode, "schedule already exists")
+	ErrInternalError     = NewCustomError(InternalErrorCode, "internal error")
+)
